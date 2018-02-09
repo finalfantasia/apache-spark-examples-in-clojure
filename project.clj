e4d804e3980a7f8a4d654bbba8b5d929759813ab
Abdussalam Abdurrahman <aabdurrahman@cj.com>
2018-02-09 14:34:59 -0800

    change directory structure to account for java source files.

--- project.clj
+++ project.clj
@@ -9,5 +9,9 @@
                  [org.apache.spark/spark-sql_2.11 "2.2.1"]]
 
   :main ^:skip-aot apache-spark-examples-in-clojure.core
+
+  :source-paths ["src/clojure"]
+  :java-source-paths ["src/java"]
   :target-path "target/%s"
+
   :profiles {:uberjar {:aot :all}})
